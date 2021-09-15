d4eec8d8342a6399dc9c5161118e2e17f684ce4f
tannerlsmith <tanner.smith@uvm.edu>
2021-09-15 11:24:00 -0400

    navbar styles, floatleft, floatright, searchbar built

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,31 @@ function Navbar(props) {
     return (
         <header className="googlebar">
             <div id="logo" class="splash">
-                <h1 class="fes-logo">Flat Earth Society&trade;</h1>
+                <div>
+                    <div class="logocontainer">
+                        <h1 class="fes-logo">Flat Earth Society&trade;</h1>
+                    </div>
+                    <div id="searchheader">
+                        <form id="srch" name="srch">
+                            <input id="srchinput">
+                            </input>
+                            <button id="srchbutton">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="topnav">
+                <ul id="leftNav">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Browse</a></li>
+                    <li><a href="">Find People</a></li>
+                </ul>
+                <ul id="rightNav">
+                    <li><a href="">Privacy</a></li>
+                    <li><a href="">Safety</a></li>
+                    <li><a href="">Sign Up</a></li>
+
+                </ul>
             </div>
         </header>
     )
