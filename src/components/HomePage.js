044bf59a9b7b036553dcdf7a53d5fa54ee903fff
tannerlsmith <tanner.smith@uvm.edu>
2021-09-16 08:36:05 -0400

    bootstrap added, news card on homepage added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function HomePage(props) {
     return (
-        <body>
-            <div class="content">
-                <div class="newsheader">
-                    <h5>Today on Flat EarthSpace</h5>
-                </div>
-            </div>
+        // <body>
+        //     <div class="content">
+        //         <div class="newsheader">
+        //             <h5>Today on Flat EarthSpace</h5>
+        //         </div>
+        //     </div>
             
-        </body>
+        // </body>
+        <Container class="container">
+            <Row>
+                <Col>
+                    <section class="box">
+                        <header class="newsheader">
+                            <h5>Today on Flat EarthSpace</h5>
+                        </header>
+                    </section>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
