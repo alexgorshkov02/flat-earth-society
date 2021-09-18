8e09b6d388f19e6bd03bb6c8aef75614d161a2ab
tannerlsmith <tanner.smith@uvm.edu>
2021-09-17 21:34:31 -0400

    debug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Footer />
-      <HomePage />
+      {/* <HomePage /> */}
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,48 +1,25 @@
-import React, { useEffect } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 
-function HomePage(props) {
+
+export default function HomePage() {
     return (
-        <div class="main-content">
-                    <section class="newscontainer">
-                        <header class="newsheader">
-                            <h5>Today at Flat EarthSpace</h5>
-                        </header>
-                    </section>
-                    <section class="logincontainer">
-                        <header class="loginheader">
-                            <h5>Log In</h5>
-                        </header>
-                        <div class="login-forms">
-                            <label>Email
-                                <input type="text">
-                                </input>
-                            </label>
-
-                            <label>Password
-                                <input type="text">
-
-                                </input>
-                            </label>
-                            <ul>
-                                <li>
-                                    <input id="checkbox" type="checkbox" id="checkbox"></input>
-                                    Remember Me
-                                </li>
-                                <li>
-                                    <button id="loginbutton">Log In</button>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </section>
-
-        </div>
-    
-    )
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 1,
+              width: 128,
+              height: 128,
+            },
+          }}
+        >
+          <Paper elevation={0} />
+          <Paper />
+          <Paper elevation={3} />
+        </Box>
+    );
 }
-
-export default HomePage
