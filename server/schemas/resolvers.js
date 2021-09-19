67694b6972e4de3228d1d05d7a70de25e245e74f
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-09-19 00:47:51 -0400

    Added initial setup for GraphQL. Updated apollo-server-express

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,13 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        user: async () => {
+            return await User.find();
+          },
+    }
+
+};
+
+module.exports = resolvers;
