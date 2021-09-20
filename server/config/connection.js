147523ad84a36d49b5a4e45975f330b915b90e73
tannerlsmith <tanner.smith@uvm.edu>
2021-09-20 18:07:21 -0400

    debug

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/flatearthsociety', {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
