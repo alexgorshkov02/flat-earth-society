472c945cc13259f2440f8496cc072c15e894970d
mtpullin <mtpullin@gmail.com>
2021-09-23 20:30:54 -0400

    added signup page browse page and login utility

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,48 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import {ApolloProvider} from '@apollo/client';
+import ApolloClient from 'apollo-boost';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import Footer from "./elements/Footer";
+import Signup from './components/Signup';
+import NoMatch from './components/noMatch';
+import Browse from './components/Browse';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
-    
+    <ApolloProvider client={client}>
+      <Router>
       <div className="page-container">
         <div className="content-wrap">
       <Navbar />
-      <HomePage />
+      <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/browse" component={Browse}/>
+              <Route exact path="/signup" component={Signup} />
+              <Route component={NoMatch} />
+            </Switch>
       </div>
       <Footer/>
     </div>
+    </Router>
+    </ApolloProvider>
   );
 }
 
